ed680a5cc4631d01e0f89439773ae15e07c7e850
Jorge-Rodrigo <jorgerodrigo.monteiro19@gmail.com>
2024-04-05 20:23:38 -0300

    feat: pages home, page contact, logo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ContactComponent, HomeComponent } from './pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: HomeComponent},
+    {path: "contato", component: ContactComponent}
+];
